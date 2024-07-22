3fd42029ac6675840bc3b65f5f426911d39ea9fd
Pratik Guragain <pratikguragain4@gmail.com>
2024-07-22 12:43:28 +0545

    New Code information

--- config.js
+++ config.js
@@ -8,4 +8,14 @@ git config --global user.email"pratikguragain@gmail.com"
 git config --list 
  ls vaneko list ko value print garney kam garxa
  ls -a vaneko hidden file pani hami le herna sakhxum
+
+ git add . le sabai lai add garxa
+
+ git status le status dekhauney kam garxa uncommit modified
+  changes to commit
+  git commit -m "yesma message lekhney ho hamle k change garim venera"
+ -m  vaneko message ho add new features or button
+ git push origin main -yes git ma hamle k k garim vanera push garney kam garxa
+
+ hii
 */
