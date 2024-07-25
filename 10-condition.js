590519a646ef99625e51ab03ad9c5dab323fc07d
Pratik Guragain <pratikguragain4@gmail.com>
2024-07-25 17:12:07 +0545

    UPDATE on And Or and Ternary Operator

--- 10-condition.js
+++ 10-condition.js
@@ -23,50 +23,82 @@ let student = [
   },
 ];
 
+// let studentStatus = (student) => {
+//   for (i = 0; i < student.length; i++) {
+//     // let name = student[i].name;
+//     //console.log(name);
 
-let studentStatus = (student) => {
-  for (i = 0; i < student.length; i++) {
-    // let name = student[i].name;
-    //console.log(name);
-
-    if (student[i].hasPaidStatus) {
-      console.log(`${student[i].name} Has Paid the fee and give exam `);
-    } else {
-      console.log(`${student[i].name} Hasnot Paid fee and Cannot give exam `);
-    }
-  }
-};
-studentStatus(student);
-
+//     if (student[i].hasPaidStatus) {
+//       console.log(`${student[i].name} Has Paid the fee and give exam `);
+//     } else {
+//       console.log(`${student[i].name} Hasnot Paid fee and Cannot give exam `);
+//     }
+//   }
+// };
+// studentStatus(student);
 
 let users = [
-    {
-      name: "ram",
-      hasPaidStatus: false,
-      hasScholarship: true,
-    },
-    {
-      name: "hari",
-      hasPaidStatus: true,
-      hasScholarship: false,
-    },
-    {
-      name: "sita",
-      hasPaidStatus: false,
-      hasScholarship: false,
-    },
-  ];
+  {
+    name: "ram",
+    hasPaidStatus: false,
+    hasScholarship: true,
+    hasAttendence: true,
+  },
+  {
+    name: "hari",
+    hasPaidStatus: true,
+    hasScholarship: false,
+    hasAttendence: false,
+  },
+  {
+    name: "sita",
+    hasPaidStatus: true,
+    hasScholarship: true,
+    hasAttendence: false,
+  },
+];
 
-let userStatus = (users) => {
+let userStatus = (users) => {mind
   
 
   for (i = 0; i < users.length; i++) {
-    if (users[i].hasPaidStatus || users[i].hasScholarship) {
-      console.log(`${users[i].name} has Able to give exm `);
-    } else {
-      console.log(`${users[i].name} hasnot Able to give exam `);
-    }
+    // if (users[i].hasPaidStatus || users[i].hasScholarship) {
+    //   console.log(`${users[i].name} has Able to give exm `);
+    // } else {
+    //   console.log(`${users[i].name} hasnot Able to give exam `);
+      if(users[i].hasAttendence && (users[i].hasPaidStatus || users[i].hasScholarship)){
+        console.log(`${users[i].name} can give Exam`);
+      }
+      // else if(users[i].hasAttendence && users[i].hasScholarship){
+      //   console.log(`${users[i].name} Has Able To give Exam`);
+      // }
+      else{
+        console.log(`${users[i].name} can not Give exam`);
+      }
+
+  }}
+userStatus(users);
+
+let user = {
+  name: "ram",
+  email: "ram@ram.com",
+  password: "ram-password",
+};
+
+let checkPassword = (checkEmail, checkPasswod) => {
+  if (user.email === checkEmail && user.password === checkPasswod) {
+    console.log(`${user.name} can able to login `);
+  } else {
+    console.log(`${user.name} cannot able to login`);
   }
 };
 
-userStatus(users);
+
+//checkPassword("ram@ram.com", "ram-password");
+//checkPassword("ram@ram.com", "ram-pasword");
+
+let checkPasswordd = (checkEmail,checkPassword) =>{
+         const message = user.email === checkEmail? `${user.name} can login` : `${user.name} cannot login`;
+         console.log(message);
+}
+checkPasswordd("ram@ram.com")
