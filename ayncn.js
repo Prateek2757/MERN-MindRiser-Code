f3a82a3978fd131c380676c43be05b9c1e4043fe
Pratik Guragain <pratikguragain4@gmail.com>
2024-08-06 20:57:04 +0545

    Update code async with promise and .then and add new file

--- ayncn.js
+++ ayncn.js
@@ -1,8 +1,36 @@
+// setTimeout((name) => {
+//   for (let i = 0; i < 10; i++) console.log("pratik");
+// }, 1000);
+
+let promise = new Promise((res, erj) => {
+  setTimeout(() => {
+       //erj("chalena")
+    //erj("Chalena")
+    res({
+      data: {
+        name: "Pratik",
+        email: "pratikgurgaian57@gmail.com",
+      },
+    });
+  }, 2000);
+});
+
+// try {
+//   let lastname = "guragain";
+//   let firstname = "Pratik";
+//   let fullname = lastname + firstname;
+//   console.log(fullname);
+// } catch (error) {
+//   console.log(error.message);
+// }
 
 
-    setTimeout((name)=>{
-        for(let i =0;i<10;i++)
-        console.log("pratik");
-},1000);
-
-
+promise
+.then((response) => {
+  console.log(response);
+  console.log(response.data.email);
+})
+.catch((error)=>{
+    console.log("Erroer Occur",error);
+})
+  
