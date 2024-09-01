2ac444e9d6a0466edf89960848cc5f5550cb56d2
Pratik Guragain <pratikguragain4@gmail.com>
2024-09-01 22:49:13 +0545

    ADD Jwt Token for more secure and add login For secure

--- Client/backend/Routes/Auth.js
+++ Client/backend/Routes/Auth.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
 const express = require("express");
 const User = require("../Modal/User");
 const Product = require("../Modal/Product");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
+
+const JWT_Secret= process.env.JWT_SECRET_KEY;
+console.log(JWT_Secret);
+
 router.get("/", (req, res) => {
   obj = {
     a: "Guragain",
@@ -28,7 +34,7 @@ router.post(
     body("email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  async(req, res) => {
+  async (req, res) => {
     /* console.log(req.body);
     const user = User(req.body);
     user.save();
@@ -38,18 +44,67 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    let user = await User.findOne({email:req.body.email})
-    if (user){
-        return res.status(400).json({errors:"user already exist"})
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ errors: "user already exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_Secret);
+      console.log(authToken);
+
+      res.json({ user, authToken });
+    } catch (error) {
+      res.status(500).send("Internal Server error");
     }
-    const salt = await bcrypt.genSalt(10)
-     secPass = await bcrypt.hash(req.body.password,salt)
-     user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: secPass
-    }).then((user) => res.json(user));
   }
 );
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+     
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+     const {email,password} = req.body
+     try {
+      let user= await User.findOne({email})
+      if(!user){
+        return res.status(400).json({errors:"user Not Found"})
+      }
+       const passwordCompare = await  bcrypt.compare(password,user.password)
+       if(!passwordCompare){
+        return res.status(400).json({errors:"password not matching"})
+       } 
+
+       const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data,JWT_Secret);
+      console.log(authToken);
+      res.json({ user, authToken });
+     } catch (error) {
+      res.status(500).send("Internal Server error");
+     }
+    })
 
 module.exports = router;
